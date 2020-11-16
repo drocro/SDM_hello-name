b871ddf789f6811ed703f233743c54cf259e1bd6
Dario Crosera <dario.cro@hotmail.it>
2020-11-16 12:01:59 +0100

    Initial commit

---
+++ src/main/java/dssc/hello/Main.java
@@ -0,0 +1,14 @@
+package dssc.hello;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length > 0){
+            System.out.println("Hello world!" + args[0]);
+        }else{
+            System.out.println("Hello Stranger!");
+        }
+    }
+}
+
+///mnt/c/Users/dario/IdeaProjects/sdm_hello-name$
